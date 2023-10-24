dd7c31fb5eb0ad12df10c0f44ebddbffb74ccdff
Adam Beji <adbej@netcompany.com>
2023-10-24 11:38:27 +0100

    next commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './net2.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://netcompany.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Netcompany official website
         </a>
       </header>
     </div>
